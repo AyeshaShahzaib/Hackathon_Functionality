61b70b106c81ddd4f7b4805537d5a5d9f1e59ea1
AyeshaShahzaib <ayeshashahzaib007@gmail.com>
2025-02-06 22:12:51 +0500

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",  
       "@typescript-eslint/no-unused-vars": "off",
       "@next/next/no-img-element": "off",
       "react/no-unescaped-entities": "off",

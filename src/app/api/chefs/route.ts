2e9d904fac5ac49790b66b380a0d29a6a7393924
AyeshaShahzaib <ayeshashahzaib007@gmail.com>
2025-02-15 11:56:51 +0500

    commit

--- src/app/api/chefs/route.ts
+++ src/app/api/chefs/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import data from '@/data/chef.json';
 
+export const dynamic = 'force-static'; // Force static generation
+
 export async function GET() {
   return NextResponse.json(data);
 }
